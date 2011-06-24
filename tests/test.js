d701a5bf50f8251b46e42d1dca07a405b6f75470
Christian Ranz <git@christianranz.com>
2011-06-24 12:26:16 +0000

    added readme and License

--- tests/test.js
+++ tests/test.js
@@ -5,24 +5,13 @@ var ds = new Cache();
 var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    if(req.url != '/favicon.ico'){
-        console.time('file request');
-        ds.file('./tests/testdata.txt',function(err, cacheElement){
-            if(err){
-                console.log(err);
-                res.end();
-            }
-            else{
-                console.log(cacheElement.size);
-                cacheElement.send(res, function(err){
-                    res.end();
-                    console.timeEnd('file request');
-                });
-                /*var utc = new Date(cacheElement.lastRead);
-                res.write(utc.toUTCString());
-                res.end();*/
-            }
-        });   
+    if(req.url == '/readfile'){
+        console.time('readFile request');
+        ds.readFile('./tests/testdata.json',function(err,data){
+            res.write(data);
+            res.end();
+            console.timeEnd('readFile request');
+        });
     }
     else{
         res.end();
